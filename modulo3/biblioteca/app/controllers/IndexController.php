34cd98e246410635a98dca1dce476bb0c0857f67
Fernando Petry <fernando.petry@autoavaliar.com.br>
2018-02-02 09:31:23 -0200

    anotacoes

--- modulo3/biblioteca/app/controllers/IndexController.php
+++ modulo3/biblioteca/app/controllers/IndexController.php
@@ -5,7 +5,62 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
+        $this->view->disable();
 
+//        $autor = Autor::findFirst(1);
+//
+//        $genero = Genero::findFirst(1);
+//
+//        $usuario = Usuario::findFirst(1);
+//        $livro = Livro::findFirst(1);
+
+
+        //-------------------- cadastro de livro
+
+
+        //        $livro = new Livro();
+        //        $livro->cd_autor = $autor->id.10;
+        //        $livro->cd_genero = $genero->id;
+        //        $livro->titulo = "Macunaima";
+        //        $livro->edicao = 10;
+        //        $livro->ano = 2000;
+        //        var_dump($livro->save());
+
+
+        //-------------------- cadastro de emprestimo
+
+        //        $emprestimo = new Emprestimo();
+        //        $emprestimo->cd_livro = $livro->id;
+        //        $emprestimo->cd_usuario = $usuario->id;
+        //        $emprestimo->data_saida = date("Y-m-d H:i:s");
+        //        var_dump($emprestimo->save());
+
+        //
+
+        $emprestimo = Emprestimo::findFirst(1);
+
+        $resultado = "Livro: " . $emprestimo->Livro->titulo . "<br>";
+        $resultado .= "Ano: " . $emprestimo->Livro->ano . "<br>";
+        $resultado .= "Edição: " . $emprestimo->Livro->edicao . "<br>";
+        $resultado .= "Autor: " . $emprestimo->Livro->Autor->nome . "<br>";
+        $resultado .= "Genero: " . $emprestimo->Livro->Genero->nome . "<br>";
+        $resultado .= "Com quem está o livro?: " . $emprestimo->Usuario->nome . "<br>";
+        $resultado .= "Data do Emprestimo: " . $emprestimo->data_saida . "<br>";
+
+
+        echo $resultado;
+
+
+        /*
+         (`id`,
+        `cd_livro`,
+        `cd_usuario`,
+        `data_saida`,
+        `data_devolucao`)
+         * */
+
+
+        exit;
     }
 
 }
